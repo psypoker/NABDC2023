554a9666898c15a0ec963249fd940435b2ba89a5
Sébastien <39156660+psypoker@users.noreply.github.com>
2023-01-15 22:50:52 +0100

    updates 2

--- NOTANOTHERBOULDERDASHCLONE/osdk_config.bat
+++ NOTANOTHERBOULDERDASHCLONE/osdk_config.bat
@@ -11,7 +11,7 @@ echo osdk_config.bat
 SET OSDKNAME=notanotherboulderdashclone
 SET OSDKFILE=buldashoric buldashoric_data buldashoric_music buldashoric_fonts buldashoric_sprites buldashoric_routines buldashoric_routines2 buldashoric_routines3
 ::        
-SET OSDKADDR=$800
+SET OSDKADDR=$500
 SET OSDKBRIEF=""
 ::%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
